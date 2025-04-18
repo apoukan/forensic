8f66247da19f00c76dae47cfac8e88b927633b3a
apoukan <37113674+apoukan@users.noreply.github.com>
2025-04-18 18:54:33 +0200

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -1,56 +1,56 @@
 #!/usr/bin/python3
+import hashlib,argparse,sys,math,os
 
-import hashlib,optparse,sys
+# Affichier l'utilisation du script
+desc = "Calcul hash de fichier\npar : Sivanesan"
+# Création de parser avec la variable utilisation
+parser=argparse.ArgumentParser(description=desc)
+parser.add_argument("-f", "--fichier", dest="fichier",help="Fichier à hasher", required=True)
+parser.add_argument("-v", "--verifier", dest="hash",help="Fichier avec Hash a vérifier")
+parser.add_argument("-m", "--methode", dest="methode",help="Méthode de hash souhaité", default="sha256")
 
-#Affichier l'utilisation du script
-utilisation = "%prog -f <fichier> [-v <fichier hash> -m <sha256 ou sha512>]"
-#Création de parser avec la variable utilisation
-parser=optparse.OptionParser(usage=utilisation)
-parser.add_option("-f", "--fichier", dest="fichier",help="Fichier à hasher")
-parser.add_option("-v", "--verifier", dest="hash",help="Fichier avec Hash a vérifier")
-parser.add_option("-m", "--methode", dest="methode",help="Méthode de hash souhaité")
+args=parser.parse_args()
 
-(options, args)=parser.parse_args()
-
-# Le fichier est une option obligatoire
-if not options.fichier:
-	print("\nCe script a besoin d'un fichier à calculer en sha256 ou en sha512\n")
-	print("Par exemple :",sys.argv[0],"-f sujet.img -m sha256\n")
-	sys.exit(1)
-
-if not options.methode:
-	print("\nCe script a besoin d'une méthode de hash a calculer\n")
-	print("Par exemple :",sys.argv[0],"-f sujet.img -m sha512\n")
-	sys.exit(1)
-
-#calcul du hash256
+# Calcul du hash256
 BLOCK = 65536
 
-if options.methode == "sha256" :
-	hasher = hashlib.sha256()
+if args.methode == "sha256" :
+    hasher = hashlib.sha256()
 else:
-	if options.methode == "sha512" :
-		hasher = hashlib.sha512()
-	else:
-		print("\nAttention méthode de hash incorrecte!")
-		sys.exit(1)
+    if args.methode == "sha512" :
+        hasher = hashlib.sha512()
+    else:
+        print("\nAttention méthode de hash incorrecte!")
+        sys.exit(1)
 
-with open(options.fichier, 'rb') as fichier:
+# Obtenir la taille totale du fichier en octets
+total_size = os.path.getsize(args.fichier)
+
+# Initialiser les variables pour suivre la progression
+hashed_size = 0
+
+with open(args.fichier, 'rb') as fichier:
     buf = fichier.read(BLOCK)
     while len(buf) > 0:
         hasher.update(buf)
+        hashed_size += len(buf)
+        percent_hashed = hashed_size / total_size
+        progress = math.floor(percent_hashed * 50)
+        print(f"\rCalcul du hash: [{'#' * progress}{'-' * (50 - progress)}] {format(percent_hashed * 100, '.1f')}%", end="")
         buf = fichier.read(BLOCK)
 
 sha=hasher.hexdigest()
-print("Le hash du fichier",options.fichier, "est :",sha)
+print(f"\nLe hash du fichier {args.fichier} est : {sha}")
 
-if options.hash:
-	with open(options.hash, 'r') as h:
-		verif=h.read().split()
-		if sha in verif:
-			print("\nLes deux hash sont identiques")
-
-		else:
-			print("\nLes deux hash ne sont pas identiques !")
-
+if args.hash:
+    with open(args.hash, 'r') as h:
+        verif=h.read().split()
+        for ligne in verif:
+            if sha.upper() in ligne.split('"'):
+                print("\nLes deux hash sont identiques")
+                sys.exit(0)
+            elif sha.lower() in ligne.split('"'):
+                print("\nLes deux hash sont identiques")
+                sys.exit(0)
+    print("\nLes deux hash ne sont pas identiques !")
 
